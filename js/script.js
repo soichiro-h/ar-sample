26c54aa4c3ad0cf7b8600c9dbd98643055dac16a
Soichi <web.pour.la.vie@gmail.com>
2024-03-09 03:58:26 +0900

    add monster

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.getElementById('viewInAR').addEventListener('click', function() {
   const anchor = document.createElement('a');
-  anchor.setAttribute('href', 'assets/sneaker_airforce.usdz');
+  anchor.setAttribute('href', 'assets/monster.usdz');
   anchor.setAttribute('rel', 'ar');
   anchor.click();
 });
